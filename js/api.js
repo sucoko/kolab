12b3646dc81835b1247dd92810c1a773cf5d6735
sucoko <45024173+sucoko@users.noreply.github.com>
2026-06-25 07:12:57 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,16 +1,44 @@
 const API_URL='https://script.google.com/macros/s/AKfycbwGMiiD73Assn_UXuvkZQSrWJp4K6_QiulkGCGKPPHNp9aBrVa9EnNseu3aVQJdDvDr/exec';
 
 async function loadCompetencies(){
- const res=await fetch(API_URL+'?action=getCompetencies');
- const data=await res.json();
- const select=document.getElementById('kompetensi');
- if(!select) return;
- data.forEach(item=>{
-   const opt=document.createElement('option');
-   opt.value=item.CompetencyID;
-   opt.textContent=item['Nama KK'];
-   select.appendChild(opt);
- });
+
+ try{
+
+   console.log("Mulai load");
+
+   const res = await fetch(API_URL+'?action=getCompetencies');
+
+   console.log("Status:", res.status);
+
+   const data = await res.json();
+
+   console.log("Data:", data);
+
+   const select = document.getElementById('kompetensi');
+
+   console.log("Select:", select);
+
+   if(!select) return;
+
+   data.forEach(item=>{
+
+     const opt = document.createElement('option');
+
+     opt.value = item.CompetencyID;
+     opt.textContent = item['Nama KK'];
+
+     select.appendChild(opt);
+
+   });
+
+   console.log("Selesai");
+
+ }catch(err){
+
+   console.error("ERROR:", err);
+
+ }
+
 }
 
 async function simpanTema(){
